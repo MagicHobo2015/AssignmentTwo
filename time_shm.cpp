10f5c9b345186d96c766d6f0f6264181f7572093
MagicHobo2015 <joshua.land6@gmail.com>
2022-02-25 13:49:46 -0800

    Finished the shared mem version, uses clock() instead of gettimeofday()

--- time_shm.cpp
+++ time_shm.cpp
@@ -7,15 +7,6 @@
  *                   comminucate with each other                                              *
  *  See the read me for further information.                                                  *
  *********************************************************************************************/
-
-/*
-2a) The first version, time_shm.c, will have the child process write the starting time to a region of shared 
-memory before it calls execvp(). After the child process terminates, the parent will read the starting 
-time from shared memory. The region of shared memory should be established before the child process 
-is forked, allowing both the parent and child processes access to the region of shared memory.
-
-*/
-
 // our includes
 #include <fcntl.h>
 #include <iostream>
@@ -26,29 +17,19 @@ is forked, allowing both the parent and child processes access to the region of 
 #include <sys/stat.h>
 #include <sys/wait.h>
 #include <unistd.h>
-
 // end includes
 
-// ProtoTypes
-char* stripCommand(char** argv);
-void* createSharedMem(size_t memSize);
-// End Proto
-static time_t* startTime = NULL;
+// One Global variable for shared mem
+static clock_t* workingTime = NULL;
 
 int main( int argc, char** argv )
-{
+{   
     // Variables
-    char* command = nullptr;
     pid_t childID;
-    time_t t = NULL;
-    time_t endTime = NULL;
-    time_t totalTime = NULL;
-    std::string test = "Hello World!";
-    // shared mem stuff
+    clock_t t = NULL;
     const int SIZE = 4096;
     const char* NAME = "mem";
     int fd;
-    // /memStuff
     // End Variables
 
     // Create some shared memory
@@ -56,7 +37,7 @@ int main( int argc, char** argv )
     // fix the size
     ftruncate(fd, SIZE);
     // map the memObject
-    startTime = (time_t*)mmap(0, SIZE, PROT_WRITE | PROT_READ, MAP_SHARED, fd, 0);
+    workingTime = (clock_t*)mmap(0, SIZE, PROT_WRITE | PROT_READ, MAP_SHARED, fd, 0);
 
     // start a child process
     childID = fork();
@@ -68,36 +49,27 @@ int main( int argc, char** argv )
         std::cout << "Something went wrong" << std::endl;
 
     } else if ( childID == 0 ) {
-        // In the child process here
-        // timestamp
-        *startTime = time(NULL);
-        sleep(.5);
-        std::cout << *startTime << std::endl;
-        // test the shared memory
+        // child process here
 
+        // timestamp
+        *(workingTime) = clock();
+
+        // run the command
+        execvp(argv[1], argv);
 
     } else {
-        // this pid could only be the parent
+        // parent
         // now wait for child to do its thing
         wait(NULL);
-        // grab endtime
-        endTime = time(NULL);
         // get the shared mem
-        t = *startTime;
+        t = *workingTime;
+        // This gives the number of clock ticks elapsed.
+        t = clock() - t;
         // unmap the mem
-        munmap(startTime, sizeof SIZE);
-        // end - begin is the time it took in ms?.
-        totalTime = endTime - t;
-        // this just prints whats in the shared mem
-        std::cout << totalTime << std::endl;
+        munmap(workingTime, sizeof SIZE);
+
+        // formats the ticks to time
+        std::cout << "\n" << "Elapsed time:\t" << ((float)t)/CLOCKS_PER_SEC << " seconds" << std::endl;
     }
     return 0;
-}
-// this will return just the command.
-char* stripCommand(char** argv) { return nullptr; }
-
-// makes shared mem the size of
-void* createSharedMem(size_t memSize) 
-{
-    return NULL;
 }
